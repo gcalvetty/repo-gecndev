4c17c0091adf750ed532290458fa26630bbe64e8
GECN-Dev <gecndev@gmail.com>
2021-09-02 17:12:50 -0400

    Actualizando a LARAVEL 8 y Boostrap con paquete JETStrap

---
+++ resources/views/layouts/tesla/tesla-modelos.blade.php
@@ -0,0 +1,65 @@
+@section('contenido')
+
+    <ul>
+        <li>Model S
+            <a href="https://www.tesla.com/es_ES/models/">Link</a>            
+            <ol>
+                <li>Gran Autonomía
+                    - Motor dual y traccion integral
+                    - 99.990 EUR
+                </li>
+                <li>Plaid
+                    - Tracción integral de triple Motor
+                    - 129.990 EUR
+                </li>
+            </ol>
+        </li>
+        <li>
+            Model 3
+            <a href="https://www.tesla.com/es_ES/model3/">Link</a>
+            <ol>
+                <li>Autonomía estándar plus
+                    - tracción trasera
+                    - 45.990 Eur.
+                </li>
+                <li>Gran Autonomía
+                    - Motor dual y tracción integral
+                    - 52.390 Eur.
+                </li>
+                <li>Performance
+                    - Motor dual y tracción integral
+                    - 62.990 Eur.
+                </li>
+            </ol>
+        </li>
+        <li>Model X
+            <a href="https://www.tesla.com/es_ES/modelx/">Link</a>
+            <ol>
+                <li>Gran Autonomía
+                    - Motor dual y tracción integral
+                    - 109.990 EUR.            
+                </li>
+                <li>
+                    Plaid
+                    - Tracción integral de triple Motor
+                    - 119.990 EUR.
+                </li>
+            </ol>            
+        </li>
+
+        <li>Model Y
+            <a href="https://www.tesla.com/es_ES/modely/">Link</a>
+            <ol>
+                <li>Gran Autonomía
+                    - Motor dual y tracción integral
+                    - 64.000 EUR.                    
+                </li>
+                <li>Performance
+                    - Motor dual y tracción integral
+                    - 70.000 EUR.
+                </li>
+            </ol>
+        </li>
+    </ul>
+    
+@endsection
